e685752a334fcf038bc976e0346ad4490c33317d
codequest48k <codequest48k@gmail.com>
2025-11-26 22:40:04 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 export default defineConfig({
+  base: '/dreieck-consultora/',
   plugins: [
     react(),
   ],
